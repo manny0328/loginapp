d48d51bd455bcce223e4b442465f8f9f75c7933b
manny0328 <manikantareddykarri6@gmail.com>
2023-02-21 04:41:35 +0000

    loginapp react code

---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import './index.css'
+
+const Login = props => {
+  const {login} = props
+  return (
+    <div>
+      <button type="button" className="login-button" onClick={login}>
+        Login
+      </button>
+    </div>
+  )
+}
+export default Login
---
+++ src/components/Logout/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import './index.css'
+
+const Logout = props => {
+  const {logout} = props
+  return (
+    <div>
+      <button type="button" className="logout-button" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  )
+}
+export default Logout 
